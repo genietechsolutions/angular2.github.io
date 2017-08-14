68043c6d68e3479c425c25e7fd36b3527d880193
anuproject <anusharao.vgd@gmail.com>
2017-08-14 13:59:10 +0530

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgClass } from '@angular/common';
+import { UpperCasePipe } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/pipes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+export class HeroBirthday2Component {
+  birthday = new Date(1988, 3, 15); // April 15, 1988
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
+}
