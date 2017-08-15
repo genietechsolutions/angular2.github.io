d358f4569dcf115270497e94f409ec84982b5c73
genietechsolutions <genie.ch668@gmail.com>
2017-08-15 07:56:39 +0530

    form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgClass } from '@angular/common';
+import { Validator } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
