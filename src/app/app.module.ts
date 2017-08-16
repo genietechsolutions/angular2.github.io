ad30ec11b60b497fe14ea7f5dca20bbd62444c06
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2017-08-16 13:28:25 +0530

    my updates at 1pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgClass } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ServiceComponent } from './service/service.component';
-import { BlogComponent } from './blog/blog.component';
-import { ContactComponent } from './contact/contact.component';
-import { PipesDemoComponent } from './pipes-demo/pipes-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ServiceComponent,
-    BlogComponent,
-    ContactComponent,
-    PipesDemoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
