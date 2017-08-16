13d32f21226a68a603fd05b26ddf1d577a7c0bc2
anuproject <anusharao.vgd@gmail.com>
2017-08-16 17:31:50 +0530

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgClass } from '@angular/common';
 import { UpperCasePipe } from '@angular/common';
-
+import { Validator } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PipesComponent } from './pipes/pipes.component';
 import { FormsComponent } from './forms/forms.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipesComponent,
-    FormsComponent
+    FormsComponent,
+    SampleComponent
   ],
   imports: [
     BrowserModule,
