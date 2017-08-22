c8b1087a362ebc28d7c3a1b92205ba690874aa2d
pradeepkaja <30947302+pradeepkaja@users.noreply.github.com>
2017-08-22 20:39:24 +0530

    regarding ngxbootstrap
    i used ngx bootstrap element(carosel) and placed in home.html to make SPA using routing in angular. i cannot customise the carosel please check it sir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, FormControl, ReactiveFormsModule   } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgClass } from '@angular/common';
-
+import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
-import { MdrivenComponent } from './mdriven/mdriven.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    MdrivenComponent
+    HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule,
+    CarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-    birthday = new Date(1988, 3, 15); // April 15, 1988
-}
+export class AppModule { }
